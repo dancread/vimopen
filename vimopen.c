9ae105adbaba705bb40576c43e36e3d817d886f6
Daniel Read <DR024625@cerner.net>
2017-05-31 18:32:39 -0500

    I have something working but I am hitting the command line limit even using a batch file

--- vimopen.c
+++ vimopen.c
@@ -1,49 +1,70 @@
 #include <windows.h>
+#include <string.h>
+#include <shlwapi.h>
 #include <stdio.h>
 
 /*
  * Consider
- * https://jira3.cerner.com/browse/EHOSTTXSRV-1025984?focusedCommentId=2691639&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-2691639
+ * https://support.microsoft.com/en-us/help/830473/command-prompt-cmd.-exe-command-line-string-limitation
  */
-#define INPUT_BUFFER_SIZE 10000
+#define INPUT_BUFFER_SIZE 100000 // Might just make MAX_PATH
 #define VIM_CONSOLE
 #ifdef VIM_CONSOLE // Using console vim
-    #define VIM_EXE "vim "
+    #define VIM_EXE "vim"
 #endif
 #ifdef VIM_GUI // Using gui vim
-    #define VIM_EXE "gvim "
+    #define VIM_EXE "gvim"
 #endif
 INT main() {
-    HANDLE hClipBoardData;
-    INT iBytesRead, iBytesWritten = 0;
-    CHAR wsInputBuffer[INPUT_BUFFER_SIZE];
-    CHAR *ptr;
-    CHAR szFullCommand[MAX_PATH+sizeof(VIM_EXE)];
+    HANDLE hTempFile;
+    INT iBytesWritten;
+    CHAR szInputBuffer[MAX_PATH];
+    CHAR szTempDirectoryPath[MAX_PATH];
+    CHAR szTempFileName[MAX_PATH];
+    CHAR szTempBatchFileName[MAX_PATH];
+    //CHAR szFullCommand[INPUT_BUFFER_SIZE+sizeof(VIM_EXE)];
     // If piped input, then pipe it to the empty file
     if(GetFileType(GetStdHandle(STD_INPUT_HANDLE)) == FILE_TYPE_PIPE){
+      // Get %TEMP% env variable
+      GetTempPath(MAX_PATH, &szTempDirectoryPath);
+      // Create unique temp bat file
+      GetTempFileName(szTempDirectoryPath, "edx", 0, szTempFileName);
+      // Change it to a *.bat file
+      lstrcpyn(szTempBatchFileName, szTempFileName, MAX_PATH);
+      szTempBatchFileName[lstrlen(szTempBatchFileName)-3]='\0';
+      lstrcat(szTempBatchFileName,"bat");
+      MoveFile(szTempFileName, szTempBatchFileName );
+      // Write temp file
+      hTempFile = CreateFile((LPTSTR) szTempBatchFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+      // Build command path to execute vim
+      //lstrcpy(szFullCommand, VIM_EXE);
+      // write vim executable -1 is to remove NULL terminator
+      WriteFile(hTempFile, VIM_EXE, strlen(VIM_EXE), &iBytesWritten, NULL);
       // Read stdin
-      do{
-         ReadFile(GetStdHandle(STD_INPUT_HANDLE), wsInputBuffer, INPUT_BUFFER_SIZE, &iBytesRead, NULL);
-      } while(iBytesRead);
-    }
-    //printf("%s\n", wsInputBuffer);
-    // Remove newlines an replace with spaces
-    ptr = wsInputBuffer;
-    while(*ptr){
-      if(*ptr=='\n' || *ptr=='\r'){
-        *ptr = ' ';
+      while (fgets(szInputBuffer, sizeof(szInputBuffer),stdin)) {
+        // Strip newline fro fgets return
+        szInputBuffer[strcspn(szInputBuffer, "\r\n")] = 0;
+        //printf("READ: %s\n", szInputBuffer);
+        PathQuoteSpaces(szInputBuffer);
+        WriteFile(hTempFile, " ", strlen(" "), &iBytesWritten, NULL);
+        WriteFile(hTempFile, szInputBuffer, strlen(szInputBuffer), &iBytesWritten, NULL);
+        //lstrcat(szFullCommand, szInputBuffer);
+        //lstrcat(szFullCommand, " ");
+        //printf("QUOTED: %s\n", szInputBuffer);
       }
-      ptr++;
-    }
+      CloseHandle(hTempFile);
+      //printf("%s\n",szFullCommand);
 #ifdef VIM_CONSOLE // Using console vim
-    // Reset stdin to FILE_TYPE_CHAR for console vim
-    freopen("CONIN$", "r", stdin);
-    // Reset stdout for pdcurses
-    freopen("CONOUT$", "w", stdout);
+      // Reset stdin to FILE_TYPE_CHAR for console vim
+      freopen("CONIN$", "r", stdin);
+      // Reset stdout for pdcurses
+      freopen("CONOUT$", "w", stdout);
 #endif
-    // Build command path and execute vim
-    lstrcpy(szFullCommand, VIM_EXE);
-    lstrcat(szFullCommand, wsInputBuffer);
-    system(szFullCommand);
-    ExitProcess(0);
+      // Execute .bat file
+      printf("%s\n",szTempBatchFileName); 
+      system(szTempBatchFileName);
+      ExitProcess(0);
+    }
+    printf("No input detected from standard in.\n");
+    ExitProcess(1);
 }
